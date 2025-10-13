5e22d5f6fd9eb2446214ff1b31034deccae51769
venu222m <145119205+venu222m@users.noreply.github.com>
2025-10-13 16:48:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,12 @@ terraform {
      source  = "hashicorp/aws"
      version = "5.50.0"
    }
- }
+  azurerm = {
+       source  = "hashicorp/azurerm"
+       version = "4.47.0"   # pick latest stable
+
+    }
+  }
 }
 
 
@@ -42,5 +47,113 @@ resource "aws_s3_bucket" "devuserbucket" {
   tags = {
     Name        = "My bucket"
     Environment = "Dev"
+  }
+}
+
+##############################################################################################
+
+provider "azurerm" {
+  features {}
+}
+#
+resource "azurerm_resource_group" "devops_rg" {
+  name     = "rg-terraform-demo1"
+  location = "East US"
+}
+
+resource "azurerm_virtual_network" "devops_vnet" {
+  name                = "devops-vnet"
+  address_space       = ["10.1.0.0/16"]
+  location            = azurerm_resource_group.devops_rg.location
+  resource_group_name = azurerm_resource_group.devops_rg.name
+
+  tags = {
+    Environment = "DevOps"
+  }
+}
+
+# Subnet
+resource "azurerm_subnet" "devops_subnet" {
+  name                 = "devops-subnet"
+  resource_group_name  = azurerm_resource_group.devops_rg.name
+  virtual_network_name = azurerm_virtual_network.devops_vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Network Interface
+# Network Interface (private IP only)
+resource "azurerm_network_interface" "devops_nic" {
+  name                = "devops-nic"
+  location            = azurerm_resource_group.devops_rg.location
+  resource_group_name = azurerm_resource_group.devops_rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.devops_subnet.id
+    private_ip_address_allocation = "Dynamic"
+    # public_ip_address_id removed
+  }
+
+  tags = {
+    Environment = "DevOps"
+  }
+}
+
+
+# Linux Virtual Machine
+resource "azurerm_linux_virtual_machine" "devops_vm" {
+  name                = "devops-vm"
+  resource_group_name = azurerm_resource_group.devops_rg.name
+  location            = azurerm_resource_group.devops_rg.location
+  size                = "Standard_B1s"
+  admin_username      = "azureuser"
+  disable_password_authentication = false
+  admin_password      = "P@ssword123!"  # ⚠️ For demo only. Use variables or Key Vault in production
+
+  network_interface_ids = [
+    azurerm_network_interface.devops_nic.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts"
+    version   = "latest"
+  }
+
+  tags = {
+    Name = "DEVOPS-VM"
+  }
+}
+
+
+# Azure Container Registry (ACR)
+resource "azurerm_container_registry" "devops_acr" {
+  name                     = "devopsacr123456"   # must be globally unique
+  resource_group_name      = azurerm_resource_group.devops_rg.name
+  location                 = azurerm_resource_group.devops_rg.location
+  sku                      = "Basic"
+  admin_enabled            = true
+
+  tags = {
+    Environment = "DevOps"
+  }
+}
+
+# Azure Storage Account
+resource "azurerm_storage_account" "devops_sa" {
+  name                     = "devopsstorage123456"   # globally unique
+  resource_group_name      = azurerm_resource_group.devops_rg.name
+  location                 = azurerm_resource_group.devops_rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    Environment = "DevOps"
   }
 }
