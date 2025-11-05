c24ab2b5393a73fb116ad93aec88f2407cd0610e
abhikunagu <113983065+abhikunagu@users.noreply.github.com>
2025-11-05 22:23:51 +0530

    Remove AWS provider and resources from main.tf
    Removed AWS provider and related resources from main.tf.

--- main.tf
+++ main.tf
@@ -1,9 +1,5 @@
 terraform {
  required_providers {
-   aws = {
-     source  = "hashicorp/aws"
-     version = "5.50.0"
-   }
    azurerm = {
        source  = "hashicorp/azurerm"
        version = "4.47.0"   # pick latest stable
@@ -12,53 +8,9 @@ terraform {
   }
 }
 
-
-provider "aws" {
- region = "us-east-1"
-}
-
-resource "aws_vpc" "this" {
- cidr_block = "10.31.0.0/16"
-}
-
-resource "aws_instance" "devops_server" {
-  ami           = "ami-04b4f1a9cf54c11d0"
-  instance_type = "t2.micro"
-  count         = 1
-
-  tags = {
-    Name = "DEVOPS"
-  }
-}
-
-# AWS ECR Repository
-resource "aws_ecr_repository" "devops_ecr" {
-  name                 = "devops-ecr-repo1"
-  image_tag_mutability = "MUTABLE"
-  tags = {
-    Environment = "DevOps"
-  }
-}
-
-# AWS S3 Bucket
-resource "aws_s3_bucket" "devuserbucket01" {
-  bucket = "my-tf-test-bucketnewone007"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
 ##############################################################################################
 provider "azurerm" {
   features {}
-
-  tenant_id       = var.azure_tenant_id
-  subscription_id = var.azure_subscription_id
-
-  # Provide client credentials explicitly (prevents CLI fallback).
-  client_id     = var.azure_client_id
-  client_secret = var.azure_client_secret
 }
 #
 resource "azurerm_resource_group" "devops_rg" {
